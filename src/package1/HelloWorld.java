5c34ee3614ac5fd62db24d19f3a56f97c3a2aa15
Padmapriyak1 <67092725+Padmapriyak1@users.noreply.github.com>
2020-06-22 15:54:41 +0530

    Update HelloWorld.java

--- src/package1/HelloWorld.java
+++ src/package1/HelloWorld.java
@@ -3,6 +3,6 @@ package package1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Testing Poll SCM");
+      
     }
 }
